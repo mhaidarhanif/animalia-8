824d824a0ce03f8cb4f1d913cbd82bf085b7e592
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-20 21:55:18 +0700

    feat: get animal by id and more animals data

--- src/data/animals.ts
+++ src/data/animals.ts
@@ -11,4 +11,96 @@ export const animals = [
     id: 3,
     name: "Cat",
   },
+  {
+    id: 4,
+    name: "Dog",
+  },
+  {
+    id: 5,
+    name: "Elephant",
+  },
+  {
+    id: 6,
+    name: "Flamingo",
+  },
+  {
+    id: 7,
+    name: "Giraffe",
+  },
+  {
+    id: 8,
+    name: "Horse",
+  },
+  {
+    id: 9,
+    name: "Iguana",
+  },
+  {
+    id: 10,
+    name: "Jaguar",
+  },
+  {
+    id: 11,
+    name: "Kangaroo",
+  },
+  {
+    id: 12,
+    name: "Lemur",
+  },
+  {
+    id: 13,
+    name: "Monkey",
+  },
+  {
+    id: 14,
+    name: "Narwhal",
+  },
+  {
+    id: 15,
+    name: "Ostrich",
+  },
+  {
+    id: 16,
+    name: "Penguin",
+  },
+  {
+    id: 17,
+    name: "Quail",
+  },
+  {
+    id: 18,
+    name: "Rabbit",
+  },
+  {
+    id: 19,
+    name: "Snake",
+  },
+  {
+    id: 20,
+    name: "Tiger",
+  },
+  {
+    id: 21,
+    name: "Uakari",
+  },
+  {
+    id: 22,
+    name: "Vulture",
+  },
+  {
+    id: 23,
+    name: "Wallaby",
+  },
+  {
+    id: 24,
+    name: "Xenopus",
+  },
+  {
+    id: 25,
+    name: "Yak",
+  },
+  {
+    id: 26,
+    name: "Zebra",
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,13 @@ app.get("/animals", (c) => {
   return c.json(animals);
 });
 
+app.get("/animals/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const animal = animals.find((animal) => animal.id === id);
+  if (!animal) return c.notFound();
+
+  return c.json(animal);
+});
+
 export default app;
