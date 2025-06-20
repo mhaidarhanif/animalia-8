798d60e89a789b23bd4ff26f905bf38cd03fe15d
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-20 21:43:03 +0700

    feat: get /animals data

---
+++ src/data/animals.ts
@@ -0,0 +1,14 @@
+export const animals = [
+  {
+    id: 1,
+    name: "Antelope",
+  },
+  {
+    id: 2,
+    name: "Bear",
+  },
+  {
+    id: 3,
+    name: "Cat",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono";
+import { animals } from "./data/animals";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({
+    ok: true,
     message: "Hello!",
   });
+});
+
+app.get("/animals", (c) => {
+  return c.json(animals);
 });
 
 export default app;
